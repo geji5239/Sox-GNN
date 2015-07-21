a5bfae7dafe6ee06f9364abea3afc57ca95789fd
gaoji <492058555@qq.com>
2015-07-21 11:09:32 +0800

    数据库连接与登录雏形

---
+++ SoxWeb/SoxWeb/DBHelper/ConnectionS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Data;
+
+namespace SoxWeb.DBHelper
+{
+    public class ConnectionS
+    {
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        public static string ConnectionString
+        {
+            get
+            {
+                string _connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+                string ConStringEncrypt = ConfigurationManager.AppSettings["ConStringEncrypt"];
+                if (ConStringEncrypt == "true")
+                {
+                    _connectionString = DESEncrypt.Decrypt(_connectionString);
+                }
+                return _connectionString;
+            }
+        }
+    }
+}
